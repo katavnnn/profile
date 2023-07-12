95e3205eaa3806b7b5dcb03b183d172eb0b71d22
katavnnn <111687886+katavnnn@users.noreply.github.com>
2023-07-12 20:11:38 +0700

    Add files via upload

---
+++ assets/js/demnguoc.js
@@ -0,0 +1,30 @@
+// Set the date for the countdown
+var countDownDate = new Date("June 27, 2024 00:00:00").getTime();
+
+// Update the countdown every second
+var x = setInterval(function() {
+
+  // Get the current date and time
+  var now = new Date().getTime();
+
+  // Calculate the distance between the current date and the countdown date
+  var distance = countDownDate - now;
+
+  // Calculate the days, hours, minutes and seconds remaining
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Update the countdown values on the HTML elements
+  document.querySelector(".days").innerHTML = days;
+  document.querySelector(".hours").innerHTML = hours;
+  document.querySelector(".minutes").innerHTML = minutes;
+  document.querySelector(".seconds").innerHTML = seconds;
+
+  // If the countdown is finished, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.querySelector(".countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
